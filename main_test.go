3279bac786400d38546b6c11c2ebe7f15824a719
Florian Lehner <dev@der-flo.net>
2022-02-21 20:02:42 +0100

    deprecate -d and introduce -e and -r
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- main_test.go
+++ main_test.go
@@ -5,39 +5,31 @@ import (
 	"testing"
 )
 
-func TestParseAdditionalExecs(t *testing.T) {
+func TestEmbedExec(t *testing.T) {
 	tests := map[string]struct {
-		input []string
+		input string
 		execs []string
 		args  [][]string
 	}{
 		"no input": {
-			input: []string{},
+			input: "",
 		},
-		"single input": {
-			input: []string{`foo:"ba zinga"`},
+		"with argument input": {
+			input: `foo:"bar"`,
 			execs: []string{
 				"foo",
-			},
-			args: [][]string{
-				{"ba", "zinga"},
-			},
-		},
-		"multiple inputs": {
-			input: []string{
-				`foo:bar`,
-				`go:"-123 -456 -789"`,
-				`date`,
-			},
-			execs: []string{
-				"foo",
-				"go",
-				"date",
 			},
 			args: [][]string{
 				{"bar"},
+			},
+		},
+		"with multiple argument inputs": {
+			input: `go:"-123 -456 -789"`,
+			execs: []string{
+				"go",
+			},
+			args: [][]string{
 				{"-123", "-456", "-789"},
-				{},
 			},
 		},
 	}
@@ -46,13 +38,16 @@ func TestParseAdditionalExecs(t *testing.T) {
 		name := name
 		tc := tc
 		t.Run(name, func(t *testing.T) {
-			execs, args, err := parseAdditionalExecs(tc.input)
-			if err != nil {
+			// Reset package global variables
+			execs = execs[:0]
+			args = args[:0]
+
+			if err := embedExec(tc.input); err != nil {
 				t.Fatalf("expected no error but got: %v", err)
 			}
 			if !reflect.DeepEqual(execs, tc.execs) {
 				t.Fatalf("expected executables did not match. "+
-					"Got: %#v\nExpected: %#v", args, tc.args)
+					"Got: %#v\nExpected: %#v", execs, tc.execs)
 			}
 			if !reflect.DeepEqual(args, tc.args) {
 				t.Fatalf("expected arguments did not match. "+
